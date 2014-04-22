5f5b59ad17450be7404ae04154996a6b66f7a953
cafe008 <253399066@qq.com>
2014-04-22 19:00:45 +0800

    format

--- src/com/Test.java
+++ src/com/Test.java
@@ -4,8 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("sssssss");
 		// test here.
-//aaaaaaaaaaaa   
-		
-		//sadfasdfas
+		// aaaaaaaaaaaa
+
+		// sadfasdfas
 	}
 }
